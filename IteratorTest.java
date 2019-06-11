643f1fdd3c06d9728317b959fccb611cb61f0de3
jackrmcshane <jackrmcshane@gmail.com>
2019-06-10 22:34:41 -0500

    iterator operations working. trying to use list.add does not work with null list

--- IteratorTest.java
+++ IteratorTest.java
@@ -5,6 +5,7 @@ public class IteratorTest{
                 LinkedList<String> myList = new LinkedList<String>();
                 LinkedList.ListIterator itr = myList.iterator();
 
+                itr.addHere("Joe");
                 myList.append("Joe");
                 myList.append("John");
                 myList.append("Maxamillion");
@@ -17,7 +18,19 @@ public class IteratorTest{
                 myList.append("Jenny");
                 myList.append("Harry");
 
+                itr.addHere("Joe");
+                itr.addHere("John");
+                itr.addHere("Maxamillion");
+                itr.addHere("Herbert");
+                itr.addHere("Angela");
+                itr.addHere("Kyle");
+                itr.addHere("Margaret");
+                itr.addHere("Marissa");
+                itr.addHere("Sally");
+                itr.addHere("Jenny");
+                itr.addHere("Harry");
 
+                itr.restart();
                 while(itr.hasNext()){
                         System.out.println(itr.next());
                 }
